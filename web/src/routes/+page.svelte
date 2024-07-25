d517b20574b5ecc3a5b7e808d5859f9219025244
Vann <111390478+Van-x2@users.noreply.github.com>
2024-07-25 03:51:26 +0000

    fixed upper mobile gui

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script>
   import { onMount } from 'svelte'
+  import { scale } from 'svelte/transition';
 
   //variables used for collecting data from input forms
     //Used for holding users username
@@ -22,9 +23,10 @@
   
   onMount(() => {
   //so the user cannot scroll the page
-  //document.body.style.overflow = 'hidden'
+  document.body.style.overflow = 'hidden'
   document.body.style.touchAction = 'none'
-  document.body.style.paddingBottom = 'env(safe-area-inset-bottom)'
+
+
 
   //variable used to reference the document outside of onMount()
   mountedDocument = document
@@ -162,32 +164,32 @@
 
 <div id="window" class="w-screen h-[100svh] bg-gray1">
   <div class="w-full h-full flex flex-col">
-    <div id="topWrapper" class=" w-full h-[270px] ">
-      <div id="nav" class="w-full h-20 relative bg-white shadow-lg z-10">
+    <div id="topWrapper" class=" w-full h-[270px]">
+      <div id="nav" class="w-full h-16 relative bg-white shadow-lg z-10">
         <div class="w-full h-full flex relative">
-          <div class="h-20 w-20 bg-primary flex items-center justify-center">
-            <span class=" text-white text-[65px] font-title font-bold italic absolute">L</span>
+          <div class="h-16 w-16 bg-primary flex items-center justify-center">
+            <span class=" text-white text-[50px] font-title font-bold italic absolute">L</span>
             <div class="w-full h-full relative">
               <div class="w-4 h-4 absolute bottom-[4px] right-[4px]">
                 <div class="w-full h-full relative">
-                  <div class="w-[4px] h-[15px] bg-white absolute bottom-0 right-0"></div>
-                  <div class="w-[15px] h-[4px] bg-white absolute bottom-0 right-0"></div>
+                  <div class="w-[4px] h-[13px] bg-white absolute bottom-0 right-0"></div>
+                  <div class="w-[13px] h-[4px] bg-white absolute bottom-0 right-0"></div>
                 </div>
               </div>
             </div>
           </div>
-          <div class="h-20 w-fit p-4 pl-6">
+          <div class="h-16 w-fit p-4 pl-4">
             <div class="w-full h-full flex items-center">
-              <p class=" text-primary font-title font-bold italic text-[35px]">
+              <p class=" text-primary font-title font-bold italic text-[27px]">
               {username}
               </p>
             </div>
           </div>
-          <div class="h-20 w-20 bg-white right-0 absolute">
+          <div class="h-16 w-16 bg-white right-0 absolute">
             <div class="w-full h-full">
               <div id="menuIcon" class="w-full h-full flex items-center justify-center transition-all duration-300">
                 <button on:click={menuIconClicked}>
-                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="size-14 fill-neutral-800 stroke-neutral-800 stroke-[0.8px]">
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="size-10 fill-neutral-800 stroke-neutral-800 stroke-[0.8px]">
                     <path d="M3 6.75A.75.75 0 0 1 3.75 6h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 6.75ZM3 12a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 12Zm0 5.25a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75a.75.75 0 0 1-.75-.75Z" clip-rule="evenodd" />
                   </svg>
                 </button>     
@@ -196,15 +198,15 @@
           </div>
         </div>
       </div>
-      <div id="menu" class="w-full h-40 flex justify-center z-9  transition-all -translate-y-40 duration-300 absolute">
-        <div class="w-[99%] h-full bg-gray2 border-b-[4px] border-x-[4px] border-neutral-700 rounded-br-[30px] rounded-bl-[30px] p-[10px]">
-          <div class="w-full h-full font-normal font-semibold text-[17px]">
-            <button class="w-full h-[44%] mt-[5px] mb-[7px]" on:click={clearUsername}>
+      <div id="menu" class="w-full h-[115px] flex justify-center z-9  transition-all -translate-y-40 duration-300 absolute">
+        <div class="w-[99%] h-full bg-gray2 border-b-[4px] border-x-[4px] border-neutral-700 rounded-br-[25px] rounded-bl-[25px] px-[8px] py-[7px]">
+          <div class="w-full h-full font-normal font-semibold text-[13px] relative">
+            <button class="w-full h-[45%]" on:click={clearUsername}>
               <div class="w-full h-full bg-neutral-800 rounded-[13px] flex text-white justify-center items-center">
                 <span>Reset Username</span>
               </div>
             </button>
-            <button class="w-full h-[44%] my-[6px]">
+            <button class="w-full h-[45%] mt-2">
               <div class="w-full h-full bg-darkred rounded-[13px] flex text-white justify-center items-center">
                 <span>Leave the Lectionary</span>
               </div>
